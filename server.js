2bf84b37657fe4624d38fd1120d16ec6f68416ad
Priyanshu Chaturvedi <messageforpriyanshu@gmail.com>
2023-04-18 11:29:07 +0530

    updated

--- server.js
+++ server.js
@@ -15,13 +15,6 @@ app.use('/api/user',userRoute);
 app.use('/api/admin',adminRoute);
 app.use('/api/doctor/',doctorsRoute);
 
-if(process.env.NODE_ENV === 'production')
-{
-    app.use('/' . express.static('client/build'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname , 'client/build/index.html'));
-    });
-}
 
 app.listen(port , () => {
     console.log(`listning on port ${port}`);
